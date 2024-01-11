01d3c90aeaaf75841651d36b3d967e9c407e4e5c
gustavo-angeli <dev.gustavosilva@outlook.com>
2024-01-11 14:16:39 -0300

    refactor: Using React useRef() instead of React useState() for input values.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css';
 import Table from './components/Table/Table';
 import CreateRowItem from './components/AddItemTable/AddItemTable'
-import { useState } from 'react';
+import { useState } from "react"
 
 export default function App() {
   const [items, setItems] = useState([])
 
-  function handleClick({task, assigned}) {
+  function handleClick(task, assigned) {
     setItems(prevList => {
       return [...prevList, { task: task, assigned: assigned }]
     })
--- src/components/AddItemTable/AddItemTable.jsx
+++ src/components/AddItemTable/AddItemTable.jsx
@@ -1,28 +1,22 @@
 import "./AddItemTable.css"
-import { useState } from "react"
+import { useRef } from "react"
 
 
 export default function AddItemTable({ onSelect }) {
-    const [item, SetItem] = useState({task: "", assigned: ""})
-
-    function handleOnChange(atribute, newValue) {
-        SetItem((prevItem) => {
-            return {
-              ...prevItem,
-              [atribute]: newValue,
-            }
-        });
-    }
+    const task = useRef();
+    const assigned = useRef();
 
     function handleOnSelect() {
-        if (item.task != "" && item.assigned != "") {
-            onSelect(item)
+        if (task.current.value.trim() !== "" && assigned.current.value.trim() !== "") {
+            onSelect(task.current.value ,assigned.current.value);
+            task.current.value = "";
+            assigned.current.value = "";
         }
     }
 
     return <div className="fields">
-        <input onChange={(event) => {handleOnChange("task", event.target.value)}} type="text" placeholder="task" maxLength={27}/>
-        <input onChange={(event) => {handleOnChange("assigned", event.target.value)}} type="text" placeholder="assigned" maxLength={27}/>
+        <input ref={task} type="text" placeholder="task" maxLength={27}/>
+        <input ref={assigned} type="text" placeholder="assigned" maxLength={27}/>
         <button onClick={handleOnSelect}>Add</button>
     </div>
 }
